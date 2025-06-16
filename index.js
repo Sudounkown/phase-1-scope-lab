54aa3e412b36a8758e00009cd66313ddd1d19e3d
Roy Koigu <roykoigu@protonmail.com>
2025-06-16 05:31:20 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+
+var customerName = "bob"
+const leastFavoriteCustomer = "goblin"
+
+var upperCaseCustomerName = () => customerName = customerName.toUpperCase(); 
+
+var setBestCustomer = () => {
+    bestCustomer = "not bob"    
+   
+}
+//setBestCustomer()
+
+var overwriteBestCustomer = () => bestCustomer = "maybe bob"
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "notagoblin"
+}
+//changeLeastFavoriteCustomer()
+//console.log(leastFavoriteCustomer)
